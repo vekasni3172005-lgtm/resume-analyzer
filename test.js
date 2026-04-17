8da391deb43b248e71a65601c423b2ff05d5aedd
vekasni3172005-lgtm <vekasni3172005@gmail.com>
2026-04-17 18:22:00 +0530

    Add initial tests to test.js

---
+++ test.js
@@ -0,0 +1,29 @@
+console.log("Running Tests...");
+
+// Test 1
+if (2 + 2 === 4) {
+  console.log("Test 1 Passed");
+} else {
+  console.log("Test 1 Failed");
+}
+
+// Test 2
+if ("resume".includes("sum")) {
+  console.log("Test 2 Passed");
+} else {
+  console.log("Test 2 Failed");
+}
+
+// Test 3
+if (typeof "node" === "string") {
+  console.log("Test 3 Passed");
+} else {
+  console.log("Test 3 Failed");
+}
+
+// Test 4
+if ([1,2,3].length === 3) {
+  console.log("Test 4 Passed");
+} else {
+  console.log("Test 4 Failed");
+}
